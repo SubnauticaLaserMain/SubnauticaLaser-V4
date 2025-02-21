f9ac9accaa929210a0817c0ee0e62f940fb5c3a8
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2025-02-21 22:17:31 +0100

    Create script-V4.lua

---
+++ script-V4.lua
@@ -0,0 +1,2 @@
+-- just a placeholder, we will add the script as soon as we announce it!
+game:GetService('Players').LocalPlayer:Kick('Please Wait for the Announcements inside the Official Discord Server!')
