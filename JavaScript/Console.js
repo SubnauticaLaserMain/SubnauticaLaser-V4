ec0e092e8bd2e57baefa4d0830e5b3e05ca21162
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2025-04-10 19:34:03 +0200

    Create Console.js

---
+++ JavaScript/Console.js
@@ -0,0 +1,10 @@
+self.postMessage({
+    type: 'styledLog',
+    text: '%cSubnauticaLaser V4.0.0 - Blooket',
+    style: 'color: #03f0fc; font-size: 200%'
+});
+
+self.postMessage({
+    type: 'normalLog',
+    text: 'Product by SubnauticaLaser!\\n\\nLICENSE: https://github.com/SubnauticaLaserMain/SubnauticaLaser-V4/blob/main/LICENSE'
+});
