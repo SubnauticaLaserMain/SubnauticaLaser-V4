f0f39ed325bdb028f22bd413370452d58f4b4811
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2025-04-10 23:05:19 +0200

    Update Console.js

--- JavaScript/Console.js
+++ JavaScript/Console.js
@@ -8,3 +8,7 @@ self.postMessage({
     type: 'normalLog',
     text: 'Product by SubnauticaLaser!\\n\\nLICENSE: https://github.com/SubnauticaLaserMain/SubnauticaLaser-V4/blob/main/LICENSE'
 });
+
+self.postMessage({
+    type: 'terminate'
+})
